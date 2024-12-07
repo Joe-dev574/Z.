cbb5b4ae246792656cfa9c51f0b9b43f7cf979c5
JDeWeese <jdeweese574@icloud.com>
2024-12-07 12:31:12 -0600

    Initial Commit

---
+++ Z./Z_App.swift
@@ -0,0 +1,32 @@
+//
+//  Z_App.swift
+//  Z.
+//
+//  Created by Joseph DeWeese on 12/7/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Z_App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Z.Tests/Z_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Z_Tests.swift
+//  Z.Tests
+//
+//  Created by Joseph DeWeese on 12/7/24.
+//
+
+import Testing
+@testable import Z_
+
+struct Z_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
